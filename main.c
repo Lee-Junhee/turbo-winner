519691cec449d2e6c827b6209cd70a6761ce6acb
Lee-Junhee <junhee.lee002@gmail.com>
2019-11-16 09:03:45 -0500

    working code

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<errno.h>
-#include<fcntl.h>
 #include<string.h>
 #include<dirent.h>
 #include<sys/stat.h>
@@ -11,23 +10,39 @@ int main() {
 
 	struct dirent *entry;
 
-	printf("----------\n");
+	printf("**********\n");
+	unsigned int total = 0;
 	while (1) {
 		entry = readdir(cwd);
 		if (!entry) {
 			break;
 		}
-		char filename[256] = entry->d_name;
-		fd = open(filename);
+		char filename[256];
+	       	strcpy(filename, entry->d_name);
 		struct stat fileinfo;
-		stat(fd, &fileinfo);
+		stat(filename, &fileinfo);
 		int size = fileinfo.st_size;
 		int mode = fileinfo.st_mode;
-		char perms[16] = "----------";
+		char perms[11] = "----------";
+		if (!(entry->d_type - 4)) {
+			strncpy(perms, "d", 1);
+		}else {
+			total += size;
+		}
+		int i;
+		char all[11] = "-rwxrwxrwx";
+		int bit = 1;
+		for(i = 9; i > 0; i--) {
+			if (bit & mode) {
+				strncpy(perms + i, all + i, 1);
+			}
+			bit *= 2;
+		}
 
 		printf("%s\t%d\t%s\n", perms, size, filename);
 	}
-	printf("----------\n");
+	printf("\nTotal Directory Size: %d bytes\n", total);
+	printf("**********\n");
 
 	return 0;
 }
