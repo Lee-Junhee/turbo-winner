0bbaf5f7bb0ec09d1d768cfbc9913f0d8a6343cf
Lee-Junhee <junhee.lee002@gmail.com>
2019-11-18 20:58:06 -0500

    working with specified dir

--- main.c
+++ main.c
@@ -5,33 +5,46 @@
 #include<sys/stat.h>
 #include<sys/types.h>
 
-int main() {
-	DIR *cwd = opendir(".");
+int main(int argc, char *argv[]) {
+	DIR *cwd;
+	char path[256];
+	if (argc == 1) {
+		strcpy(path, ".");
+	}else {
+		strcpy(path, argv[1]);
+	}
+	cwd = opendir(path);
 
 	struct dirent *entry;
 
 	printf("**********\n");
 	unsigned int total = 0;
+	char filename[256];
+	char full[512];
+	struct stat fileinfo;
+	int size, mode, i, bit;
+	char perms[11];
+	char none[11] = "----------";
+	char all[11] = "-rwxrwxrwx";
 	while (1) {
 		entry = readdir(cwd);
 		if (!entry) {
 			break;
 		}
-		char filename[256];
 	       	strcpy(filename, entry->d_name);
-		struct stat fileinfo;
-		stat(filename, &fileinfo);
-		int size = fileinfo.st_size;
-		int mode = fileinfo.st_mode;
-		char perms[11] = "----------";
+		strcpy(full, path);
+		strcat(full, "/");
+		strcat(full, filename);
+		stat(full, &fileinfo);
+		size = fileinfo.st_size;
+		mode = fileinfo.st_mode;
+		strcpy(perms, none);
 		if (!(entry->d_type - 4)) {
 			strncpy(perms, "d", 1);
 		}else {
 			total += size;
 		}
-		int i;
-		char all[11] = "-rwxrwxrwx";
-		int bit = 1;
+		bit = 1;
 		for(i = 9; i > 0; i--) {
 			if (bit & mode) {
 				strncpy(perms + i, all + i, 1);
